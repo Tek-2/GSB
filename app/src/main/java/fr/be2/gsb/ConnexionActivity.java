74f4a41a43faf7338e9808d1f936bd215fff2ea6
Tek-2 <tehilaelcs@gmail.com>
2023-01-15 15:45:58 +0100

    Initial commit

---
+++ app/src/main/java/fr/be2/gsb/ConnexionActivity.java
@@ -0,0 +1,14 @@
+package fr.be2.gsb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+    public class ConnexionActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_connexion);
+        }
+    }
